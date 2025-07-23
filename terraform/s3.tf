c9aa2d8677d87754b01864eab784d9e98c27541d
Brunosg9 <brunog.silva@live.com>
2025-07-23 20:56:53 -0300

    project-casamento

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,18 +1,21 @@
 resource "aws_s3_bucket" "wedding_site" {
-  bucket = "fotos-casamento-bruno-vanessa"
+  bucket = var.s3.bucket_name
 
-  tags = {
-    Name = "Wedding Site S3"
-  }
+  tags = merge(
+    {
+      Name = "Wedding Site S3"
+    },
+    var.s3.tags
+  )
 }
 
 resource "aws_s3_bucket_public_access_block" "wedding_site" {
   bucket = aws_s3_bucket.wedding_site.id
 
-  block_public_acls       = true
-  block_public_policy     = false
-  ignore_public_acls      = true
-  restrict_public_buckets = false
+  block_public_acls       = var.s3_public_access_block.block_public_acls
+  block_public_policy     = var.s3_public_access_block.block_public_policy
+  ignore_public_acls      = var.s3_public_access_block.ignore_public_acls
+  restrict_public_buckets = var.s3_public_access_block.restrict_public_buckets
 }
 
 resource "aws_s3_bucket_website_configuration" "wedding_site" {
@@ -26,10 +29,10 @@ resource "aws_s3_bucket_website_configuration" "wedding_site" {
     key = "error.html"
   }
 
-    cors_rule {
-        allowed_headers = ["*"]
-        allowed_methods = ["GET", "PUT", "POST"]
-        allowed_origins = ["*"]
-        expose_headers  = ["ETag"]
-    }
+  cors_rule {
+    allowed_headers = ["*"]
+    allowed_methods = ["GET", "PUT", "POST"]
+    allowed_origins = ["*"]
+    expose_headers  = ["ETag"]
+  }
 }
---
+++ terraform/variables.tf
@@ -0,0 +1,29 @@
+variable "s3" {
+  description = "Configurações do bucket S3"
+  type = object({
+    bucket_name = string
+    region      = string
+    tags        = map(string)
+  })
+  default = {
+    bucket_name = ""
+    region      = "us-east-1"
+    tags        = {}
+  }
+}
+
+variable "s3_public_access_block" {
+  description = "Configurações de acesso público do bucket S3"
+  type = object({
+    block_public_acls       = bool
+    block_public_policy     = bool
+    ignore_public_acls      = bool
+    restrict_public_buckets = bool
+  })
+  default = {
+    block_public_acls       = true
+    block_public_policy     = false
+    ignore_public_acls      = true
+    restrict_public_buckets = false
+  }
+}
